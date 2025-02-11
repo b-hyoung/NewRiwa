1993ed57e9870636fbceeb97c4940c041b39c354
b-hyoung <youqlrqod@gmail.com>
2025-02-11 13:22:44 +0900

    add MongoDb

--- backend_bob/api/models.py
+++ backend_bob/api/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from pymongo import MongoClient
+
+# 몽고DB 연결하기
+client = MongoClient("mongodb://localhost:27017/")
+db = client("kimbob") ## 사용 할 Mongo DB 데이터 베이스
+collection = db["item"] #사용 할 컬렉션
+
 
 class UserInfo(models.Model):
     username = models.CharField(max_length=10)
